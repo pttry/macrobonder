1d3ff064f54aeb61b9230210dcfdb5321f0c36d8
Janne Huovari <janne.huovari@ptt.fi>
2017-03-26 14:39:13 +0300

    get and read. currency and dates for read.

---
+++ R/get_mb.R
@@ -0,0 +1,31 @@
+#' Get series from Macrobond in a data.frame.
+#'
+#' The function is used to get one or several time series of same frequency from
+#' the Macrobond.
+#'
+#' For time series of different frequnecies use \code{\link{read_mb}}.
+#'
+#' @param series a vector of Macrobond serie codes.
+#'
+#' @return A data.frame with dates in \code{time} column.
+#' Time series are on other columns.
+#'
+#' @examples
+#'   x <- get_mb(series = c("usgdp", "figdp"))
+
+
+get_mb <- function(series){
+  ser_lst <- FetchTimeSeries(series)
+  freqs <- unlist(lapply(ser_lst, getFrequency))
+  if (!(all(freqs == mean(freqs)))) stop(
+    "Series have different frequencies.\n",
+    names(freqs),
+    "\nUse read_mb() instead")
+
+  xts_lst <- lapply(ser_lst, as.xts)
+
+  res <- as.data.frame(do.call("merge", xts_lst))
+  res <- data.frame(time = as.Date(rownames(res)), res)
+  row.names(res) <- NULL
+  res
+}
--- R/read_mb.R
+++ R/read_mb.R
@@ -1,22 +1,53 @@
-#' Read series from Macrobond in a data.frame.
+#' Read series from Macrobond to a data.frame.
 #'
-#' @param series a vector of Macrobond serie codes.
+#' The function is used to read one or several time series from
+#' the Macrobond. If time series have different frequency they are converted
+#' to same frequency. Other conversions in macrobond are also available.
 #'
-#' @examples x <- read_mb(series = c("usgdp", "figdp"))
+#' For time series of same frequnecy the \code{\link{read_mb}} is faster.
+#'
+#' @param series A vector of Macrobond serie codes.
+#' @param frequency An option for frequncy conversion. Default is \code{Highest},
+#'   for others see \code{\link[MacrobondAPI]{TimeSeriesFrequency}}.
+#' @param currency An option for currency conversion. Default is \code{""}
+#'   (no conversion), for conversions use currency codes ("USD", "EUR", etc).
+#'   List of supported currencies: \url{https://www.macrobond.com/index.php/currencylist}.
+#' @param start_date, A starting date. A format of available absolute and
+#'   relative date formulations see \code{\link[MacrobondAPI]{SeriesRequest-class}}
+#' @param end_date, A ending date. A format of available absolute and
+#'   relative date formulations see \code{\link[MacrobondAPI]{SeriesRequest-class}}
+#'
+#' @return A data.frame with dates in \code{time} column.
+#' Time series are on other columns.
+#'
+#' @examples
+#'   xx <- read_mb(series = c("usgdp", "uscpi"))
+#'   yy <- read_mb(series = c("usgdp", "uscpi"), frequency = "Annual")
+  zz <- read_mb(series = c("usnaac0057", "eunaac0019"),
+                frequency = "Annual", currency = "USD",
+                start_date = "", end_date = ""))
+  library(jtools)
+zz %>%
+  gather(vars, values, -time) %>%
+  ggplot(aes(time, values, colour = vars)) +
+  geom_line()
 
 
-read_mb <- function(series){
-  ser_lst <- FetchTimeSeries(series)
-  freqs <- unlist(lapply(ser_lst, getFrequency))
-  if (!(all(freqs == mean(freqs)))) stop(
-    "Series have different frequencies.\n",
-    names(freqs))
+read_mb <- function(series, frequency = "Highest", currency = "",
+                    dates ){
+
+  seriesRequest <- CreateUnifiedTimeSeriesRequest()
+  setFrequency(seriesRequest, TimeSeriesFrequency[[frequency]])
+  setCurrency(seriesRequest, currency = currency)
+  setStartDate(seriesRequest, startDate = start_date)
+  serie_expressions <- lapply(series, addSeries, m = seriesRequest)
+
+  ser_lst <- FetchTimeSeries(seriesRequest)
+
   xts_lst <- lapply(ser_lst, as.xts)
 
   res <- as.data.frame(do.call("merge", xts_lst))
-  res$time <- as.Date(rownames(res))
+  res <- data.frame(time = as.Date(rownames(res)), res)
   row.names(res) <- NULL
   res
 }
-
-
